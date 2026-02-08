813c012943ea1538d2b25660cb6940a18ae90576
fakecoder <iamlishang@gmail.com>
2026-02-08 22:09:26 +0800

    feat: 统一项目版本号为 1.0.0 并优化国内多渠道打包逻辑
    - 统一 package.json, app.json, Info.plist 及 UI 界面版本号为 1.0.0
    - 优化 Android 多渠道配置，解决国内渠道 (domestic) 因缺少 Firebase 导致的编译问题
    - 在 build.gradle 中集成阿里云镜像源，提升国内构建速度
    - 完善 CrashReport 桥接模块，支持动态渠道初始化

--- android/app/src/main/java/com/anonymous/soundtherapyapp/CrashReportModule.kt
+++ android/app/src/main/java/com/anonymous/soundtherapyapp/CrashReportModule.kt
@@ -3,8 +3,6 @@ package com.anonymous.soundtherapyapp
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.google.firebase.crashlytics.FirebaseCrashlytics
-import com.tencent.bugly.crashreport.CrashReport
 
 class CrashReportModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -16,9 +14,23 @@ class CrashReportModule(reactContext: ReactApplicationContext) : ReactContextBas
     fun logException(message: String) {
         val channel = BuildConfig.CHANNEL
         if (channel == "googlePlay") {
-            FirebaseCrashlytics.getInstance().recordException(Exception(message))
+            try {
+                val clazz = Class.forName("com.google.firebase.crashlytics.FirebaseCrashlytics")
+                val method = clazz.getMethod("getInstance")
+                val instance = method.invoke(null)
+                val recordMethod = clazz.getMethod("recordException", Throwable::class.java)
+                recordMethod.invoke(instance, Exception(message))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         } else if (channel == "domestic") {
-            CrashReport.postCatchedException(Exception(message))
+            try {
+                val clazz = Class.forName("com.tencent.bugly.crashreport.CrashReport")
+                val method = clazz.getMethod("postCatchedException", Throwable::class.java)
+                method.invoke(null, Exception(message))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
@@ -26,9 +38,23 @@ class CrashReportModule(reactContext: ReactApplicationContext) : ReactContextBas
     fun setUserId(userId: String) {
         val channel = BuildConfig.CHANNEL
         if (channel == "googlePlay") {
-            FirebaseCrashlytics.getInstance().setUserId(userId)
+            try {
+                val clazz = Class.forName("com.google.firebase.crashlytics.FirebaseCrashlytics")
+                val method = clazz.getMethod("getInstance")
+                val instance = method.invoke(null)
+                val setUserIdMethod = clazz.getMethod("setUserId", String::class.java)
+                setUserIdMethod.invoke(instance, userId)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         } else if (channel == "domestic") {
-            CrashReport.setUserId(userId)
+            try {
+                val clazz = Class.forName("com.tencent.bugly.crashreport.CrashReport")
+                val method = clazz.getMethod("setUserId", String::class.java)
+                method.invoke(null, userId)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
