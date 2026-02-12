4edc8438aa09abed3bc53ecc278e5a2d3cbde5d9
fakecoder <iamlishang@gmail.com>
2026-02-12 22:48:13 +0800

    fix: 统一包名为 com.anonymous.soundtherapyapp 并重新生成 Google Play AAB

--- android/app/src/main/java/com/anonymous/soundtherapyapp/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/soundtherapyapp/MainApplication.kt
@@ -2,7 +2,6 @@ package com.anonymous.soundtherapyapp
 
 import android.app.Application
 import android.content.res.Configuration
-import com.anonymous.soundtherapyapp.google.BuildConfig
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
