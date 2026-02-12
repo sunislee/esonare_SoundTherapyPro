7e2d35e77cda99fbe13a3464d1e891829915a94d
fakecoder <iamlishang@gmail.com>
2026-02-13 07:56:59 +0800

    chore: release v1.0.3 stable for Google Play

--- src/fix_metro_ids.js
+++
@@ -1,8 +0,0 @@
-// 强制占用前几个资源 ID，防止业务图片与库图片冲突
-console.log('Fixing Metro IDs...');
-const id1 = require('./assets/images/loading_icon.webp');
-const id2 = require('./assets/images/loading_icon.webp');
-const id3 = require('./assets/images/loading_icon.webp');
-const id4 = require('./assets/images/loading_icon.webp');
-const id5 = require('./assets/images/loading_icon.webp');
-console.log('Fixed IDs:', id1, id2, id3, id4, id5);
