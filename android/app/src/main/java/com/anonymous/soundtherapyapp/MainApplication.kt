630853e5ae33b3e2e76d7b33ef2b4465ae27239c
fakecoder <iamlishang@gmail.com>
2026-02-12 22:35:58 +0800

    feat: 针对 Google Play 优化 UI，移除国内备案号显示。

--- android/app/src/main/java/com/anonymous/soundtherapyapp/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/soundtherapyapp/MainApplication.kt
@@ -2,6 +2,7 @@ package com.anonymous.soundtherapyapp
 
 import android.app.Application
 import android.content.res.Configuration
+import com.anonymous.soundtherapyapp.google.BuildConfig
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
